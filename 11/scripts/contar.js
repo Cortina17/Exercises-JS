d7c6d05b88cd36ad36ca914cbb9993380da828d4
Cortina17 <diego.cortinagarcia@gmail.com>
2022-02-02 18:59:57 +0100

    added base files and html structure

--- 11/scripts/contar.js
+++ 11/scripts/contar.js
@@ -2,32 +2,44 @@
 //recorrer el string. Cuando se encuentra una vocal, se recupera y se llevan a un array
 //Checkar cuantas veces aparece cada letra en el array    
 
-function countVowels(str) {
+// function countVowels(str) {
 
-    // let vowels = ['a' == 0, 'e' == 0, 'i' == 0, 'o' == 0, 'u' == 0];
-    let vowObj = {
-        'a': 0,
-        'e': 0,
-        'i': 0,
-        'o': 0,
-        'u': 0
-    }
+//     // let vowels = ['a' == 0, 'e' == 0, 'i' == 0, 'o' == 0, 'u' == 0];
+//     let vowObj = {
+//         'a': 0,
+//         'e': 0,
+//         'i': 0,
+//         'o': 0,
+//         'u': 0
+//     }
 
-    for (let i = 0; i < str.length; i++) {
+//     for (let i = 0; i < str.length; i++) {
 
-        if (str.includes('a')) {
-            vowObj.a++;
-        } else if (str.includes('e')) {
-            vowObj.e++;
-        } else if (str.includes('i')) {
-            vowObj.i++;
-        } else if (str.includes('o')) {
-            vowObj.o++;
-        } else if (str.includes('u')) {
-            vowObj.u++;
+//         if (str.includes('a')) {
+//             vowObj.a++;
+//         } else if (str.includes('e')) {
+//             vowObj.e++;
+//         } else if (str.includes('i')) {
+//             vowObj.i++;
+//         } else if (str.includes('o')) {
+//             vowObj.o++;
+//         } else if (str.includes('u')) {
+//             vowObj.u++;
 
-        }
-        return vowObj;
-    }
+//         }
+//         return vowObj;
+//     }
+// }
+
+function countVowels(phrase){
+
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+
+            
+    
 }
+
+
+
+
 module.exports = countVowels;
