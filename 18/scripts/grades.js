48defff5807ac15612ba9df649bbb85ce628230f
Cortina17 <diego.cortinagarcia@gmail.com>
2022-02-07 19:42:18 +0100

    Ex Nº18 test passed

---
+++ 18/scripts/grades.js
@@ -0,0 +1,16 @@
+function showGrades(num){
+
+    if(num >= 9){
+        return 'Outstanding';
+    } else if(num < 9 && num >= 7){
+        return 'Remarkable';
+    } else if(num < 7 && num >= 5){
+        return 'Suficient';
+    }else if(num < 5 && num >= 3){
+        return 'Insuficient';
+    } else if(num < 3 && num >= 0){
+        return 'Too poor'
+    }
+}
+
+module.exports = showGrades;
--- 18/tests/gradesTest.test.js
+++ 18/tests/gradesTest.test.js
@@ -6,24 +6,21 @@ describe('test', () => {
         expect(grades(0)).toBe('Too poor');
         expect(grades(1)).toBe('Too poor');
         expect(grades(2)).toBe('Too poor');
-        expect(grades(3)).toBe('Too poor');
     });
 
     test('Test if 3-5 shows insuficient', () => {
         expect(grades(3)).toBe('Insuficient');
         expect(grades(4)).toBe('Insuficient');
-        expect(grades(5)).toBe('Insuficient');
     });
 
-    test('Test if 6-7 shows good', () => {
+    test('Test if 6-7 shows suficient', () => {
+        expect(grades(5)).toBe('Suficient');
         expect(grades(6)).toBe('Suficient');
-        expect(grades(7)).toBe('Suficient');
     });
 
     test('Test if 7-9 shows remarkable', () =>{
         expect(grades(7)).toBe('Remarkable');
         expect(grades(8)).toBe('Remarkable');
-        expect(grades(9)).toBe('Remarkable');
     });
 
     test('Test if 9-10 shows outstanding', () => {
