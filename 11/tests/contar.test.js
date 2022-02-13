81b8e699f77911c46f6290405d235fcf5d48f8b3
Cortina17 <diego.cortinagarcia@gmail.com>
2022-02-13 21:25:18 +0100

    frustration

--- 11/tests/contar.test.js
+++ 11/tests/contar.test.js
@@ -1,15 +1,14 @@
-const countvowels = require('../scripts/contar.js')
+const countVowels = require('../scripts/contar.js')
 
 describe('test', () => {
 
     test('Test if every vowel is counted', () => {
-        // expect(countvowels('hello')).toBe(['a == 0', 'e == 1', 'i == 0', 'o == 1', 'u == 0']);
-        expect(countvowels('deLUsiOnaL')).toBe({'a':1, 'e':1, 'i':1, 'o':1, 'u':1});
-        expect(countvowels('deLUsiOnaL')).toBe(['a = 1', 'e = 1', 'i = 1', 'o = 1', 'u = 1']);
-        expect(countvowels('cmptr scnc')).toBe('There are no vowels in this phrase!');
+        expect(countVowels('hello')).toBe([0, 1, 0, 1, 0]);
+        // expect(countVowels('deLUsiOnaL')).toBe(['a = 1', 'e = 1', 'i = 1', 'o = 1', 'u = 1']);
+        // expect(countVowels('cmptr scnc')).toBe('There are no vowels in this phrase!');
     });
     test('Test if each vowel is counted the right amount of times', () => {
-        expect(countvowels('abracadabra')).toBe(['a = 5', 'e = 0', 'i = 0', 'o = 0', 'u = 0']);
-        expect(countvowels('HaLLuCInogEniC')).toBe(['a = 1', 'e = 1', 'i = 2', 'o = 1', 'u = 1']);
+        expect(countVowels('abracadabra')).toBe(['a = 5', 'e = 0', 'i = 0', 'o = 0', 'u = 0']);
+        expect(countVowels('HaLLuCInogEniC')).toBe(['a = 1', 'e = 1', 'i = 2', 'o = 1', 'u = 1']);
     });
 });
