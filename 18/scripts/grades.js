074887762c38a608c76422f118b79ae3a61def34
Cortina17 <diego.cortinagarcia@gmail.com>
2022-02-14 10:57:47 +0100

    first refactor tryout

--- 18/scripts/grades.js
+++ 18/scripts/grades.js
@@ -1,16 +1,11 @@
 function showGrades(num){
 
-    if(num >= 9){
-        return 'Outstanding';
-    } else if(num < 9 && num >= 7){
-        return 'Remarkable';
-    } else if(num < 7 && num >= 5){
-        return 'Suficient';
-    }else if(num < 5 && num >= 3){
-        return 'Insuficient';
-    } else if(num < 3 && num >= 0){
-        return 'Too poor';
-    }
+    if (num >= 0 && num <= 10){
+
+        return num >= 9 ? 'Outstanding' : num < 9 && num >= 7 ? 'Remarkable' : num < 7 && num >= 5 ? 'Suficient' : num < 5 && num >= 3 ? 'Insuficient' : 'Too poor'
+
+    } return 'Enter a number between 0 and 10'
+
 }
 
 module.exports = showGrades;
--- 18/tests/gradesTest.test.js
+++ 18/tests/gradesTest.test.js
@@ -2,6 +2,11 @@ const grades = require('../scripts/grades.js')
 
 describe('test', () => {
 
+    test('Test if the number is between 0 and 10', () => {
+        expect(grades(-10)).toBe('Enter a number between 0 and 10');
+        expect(grades(17)).toBe('Enter a number between 0 and 10');
+    });
+
     test('Test if 0-3 shows too poor', () => {
         expect(grades(0)).toBe('Too poor');
         expect(grades(1)).toBe('Too poor');
